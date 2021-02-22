73b60a7fb201e46774811711a5c0f14a70cf5b51
ysb <seungbin9850@gmail.com>
2021-02-22 16:26:26 +0900

    [ADD] use global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { sequelize } from './config/database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await sequelize.sync();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
