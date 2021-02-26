6adc0f6c66b19c790d03629dc1cce7d2b63ad986
seungbin9850 <tanja1206@naver.com>
2021-02-26 16:38:31 +0900

    [DEL] utils

--- src/utils/crypto.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt-nodejs';
-
-export const passwordEncoding = async (password: string): Promise<string> => {
-  console.log(11);
-  return await bcrypt.hashSync(password, 'qawsed');
-};
